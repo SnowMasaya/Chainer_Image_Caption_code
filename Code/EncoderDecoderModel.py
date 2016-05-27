d06a0f3518d92abb1557aca081e5c29f1c9992eb
SnowGushiGit <snow.akogi.pgel@gmail.com>
2016-05-27 23:06:43 +0900

    update model

--- Code/EncoderDecoderModel.py
+++ Code/EncoderDecoderModel.py
@@ -51,62 +51,92 @@ class EncoderDecoderModel:
         x = XP.farray(self.id2image.data)
         return self.encdec.encode(x)
 
-    def __forward_word(self):
-        t = XP.iarray([self.trg_stoi('<s>') for _ in range(self.batch_size)])
+    def __forward_word(self, trg_batch, trg_vocab, encdec, is_training, generation_limit):
+        trg_stoi = trg_vocab.stoi
+        trg_itos = trg_vocab.itos
+        t = XP.iarray([trg_stoi('<s>') for _ in range(self.batch_size)])
         hyp_batch = [[] for _ in range(self.batch_size)]
-        if self.is_training:
+        trg_len = len(trg_batch[0]) if trg_batch else 0
+        if is_training:
             loss = XP.fzeros(())
-            for l in range(self.trg_len):
-                y = self.encdec.decode(t)
-                t = XP.iarray([self.trg_stoi(self.trg_batch[k][l]) for k in range(self.batch_size)])
+            for l in range(trg_len):
+                y = encdec.decode(t)
+                t = XP.iarray([trg_stoi(self.trg_batch[k][l]) for k in range(self.batch_size)])
                 loss += functions.softmax_cross_entropy(y, t)
                 output = cuda.to_cpu(y.data.argmax(1))
                 for k in range(self.batch_size):
-                    hyp_batch[k].append(self.trg_itos(output[k]))
+                    hyp_batch[k].append(trg_itos(output[k]))
             return loss, hyp_batch
+        else:
+            while len(hyp_batch[0]) < generation_limit:
+                y = self.encdec.decode(t)
+                output = cuda.to_cpu(y.data.argmax(1))
+                t = self.common_function.my_array(output, np.int32)
+                for k in range(batch_size):
+                    hyp_batch[k].append(trg_itos(output[k]))
+                if all(hyp_batch[k][-1] == '</s>' for k in range(batch_size)):
+                    break
 
-    def forward(self, is_training, generation_limit, epoch):
-        self.is_training = is_training
-        self.generation_limit = generation_limit
-
-        for trg_batch in self.gen:
-            self.trg_batch = fill_batch(trg_batch)
-            if len(self.trg_batch) != self.minibatch:
-                break
-            self.batch_size = len(self.trg_batch)
-            self.trg_len = len(self.trg_batch[0]) if self.trg_batch else 0
-            self.encdec.clear(self.batch_size)
-            self.__forward_img()
-            self.encdec.reset(self.batch_size)
-            loss, hyp_batch = self.__forward_word()
-            loss.backward()
-            self.opt.update()
-            K = len(self.trg_batch) - 2
-            self.print_out(K, hyp_batch, epoch)
+        return hyp_batch
 
     def train(self):
         trace('making vocabularies ...')
-        self.trg_vocab = Vocabulary.new(gens.word_list(self.target), self.vocab)
-        self.trg_stoi = self.trg_vocab.stoi
-        self.trg_itos = self.trg_vocab.itos
+        trg_vocab = Vocabulary.new(gens.word_list(self.target), self.vocab)
 
         trace('making model ...')
 
         for epoch in range(self.epoch):
             trace('epoch %d/%d: ' % (epoch + 1, self.epoch))
-            self.opt = optimizers.AdaGrad(lr=0.01)
-            self.opt.setup(self.encdec)
-            self.opt.add_hook(optimizer.GradientClipping(5))
+            opt = optimizers.AdaGrad(lr=0.01)
+            opt.setup(self.encdec)
+            opt.add_hook(optimizer.GradientClipping(5))
             gen1 = gens.word_list(self.target)
-            self.gen = gens.batch(gen1, self.minibatch)
-            self.forward(True, 0, epoch)
+            gen = gens.batch(gen1, self.minibatch)
 
+            for trg_batch in gen:
+                self.trg_batch = fill_batch(trg_batch)
+                if len(self.trg_batch) != self.minibatch:
+                    break
+                self.batch_size = len(self.trg_batch)
+                self.trg_len = len(self.trg_batch[0]) if self.trg_batch else 0
+                self.encdec.clear(self.batch_size)
+                self.__forward_img(True)
+                self.encdec.reset(self.batch_size)
+                loss, hyp_batch = self.__forward_word(trg_batch, trg_vocab, encdec, True, 0)
+                loss.backward()
+                opt.update()
+                K = len(self.trg_batch) - 2
+            self.print_out(K, hyp_batch, epoch)
 
         trace('saving model ...')
         prefix = self.model
         self.trg_vocab.save("model/" + prefix + '.trgvocab')
         self.encdec.save_spec("model/" + prefix + '.spec')
         serializers.save_hdf5("model/" + prefix + '.weights', self.encdec)
+
+        trace('finished.')
+
+    def test(self):
+        trace('loading model ...')
+        self.trg_vocab = Vocabulary.load(self.model + '.trgvocab')
+        encdec = EncoderDecoder.load_spec(self.model + '.spec')
+        serializers.load_hdf5(self.model + '.weights', encdec)
+
+        trace('generating translation ...')
+        generated = 0
+
+        with open(self.target, 'w') as fp:
+            self.encdec.clear(self.batch_size)
+            self.__forward_img()
+            self.encdec.reset(self.batch_size)
+            trace('sample %8d ...' % (generated + 1))
+            hyp_batch = self.__forward_word(trg_batch, trg_vocab, encdec, False, self.generation_limit)
+
+            for hyp in hyp_batch:
+                hyp.append('</s>')
+                hyp = hyp[:hyp.index('</s>')]
+                print('hyp : ' +''.join(hyp))
+                print(' '.join(hyp), file=fp)
 
         trace('finished.')
 
